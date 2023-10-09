e46095a584fd4662b396a0e616daf013f1ab08dc
Olivier Daire <daire.olivier@gmail.com>
2023-10-09 17:10:08 +0200

    feat: add end to end test case

--- server/config.example.js
+++ server/config.example.js
@@ -3,6 +3,8 @@ const config = {
   CLIENT_SECRET: '',
   SCOPE: '',
   REDIRECT_URI: '',
+  QWAC_KEY_PATH: `${__dirname}/certificates/QWAC_KEY.pem`,
+  QWAC_CERT_PATH: `${__dirname}/certificates/QWAC_CERT.pem`,
 };
 
 module.exports = config;
