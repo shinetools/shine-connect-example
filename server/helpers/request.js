dd3b18f0201135ab7e4a1418d002d9b8a94c5cf8
lrbh10 <rabah.laouadi@shine.fr>
2024-01-04 13:35:40 +0100

    feat(poc): remove dsp2 and mtls layer from the  example

--- server/helpers/request.js
+++ server/helpers/request.js
@@ -1,70 +1,15 @@
-const https = require('https');
-const fs = require('fs');
-const httpSignature = require('http-signature');
+const https = require('http');
 
-const config = require('../config');
-const { generateDigest } = require('./digest');
 const RequestError = require('./requestError');
 
 const SHINE_CONNECT_PRODUCTION_HOST = 'connect.api.shine.fr';
-const SHINE_CONNECT_STAGING_HOST = 'connect.api.staging.shine.fr';
+const SHINE_CONNECT_STAGING_HOST = 'localhost';
+
 const isDev = process.env.NODE_ENV !== 'production';
 
 const SHINE_CONNECT_HOST = isDev
   ? SHINE_CONNECT_STAGING_HOST
   : SHINE_CONNECT_PRODUCTION_HOST;
-
-const QWAC_KEY = fs.readFileSync(config.QWAC_KEY_PATH);
-const QSEAL_KEY = fs.readFileSync(config.QSEAL_KEY_PATH);
-const QWAC_CERT = fs.readFileSync(config.QWAC_CERT_PATH);
-const ROOT_CERT = fs.readFileSync(config.ROOT_CA_PATH);
-
-const getRequiredQsealHeaders = () => [
-  '(request-target)',
-  'date',
-  'psu-ip-address',
-  'psu-ip-port',
-  'psu-http-method',
-  'psu-date',
-  'psu-user-agent',
-  'psu-referer',
-  'psu-accept',
-  'psu-accept-charset',
-  'psu-accept-encoding',
-  'psu-accept-language',
-];
-
-const signRequest = (request) => {
-  const qsealHeaders = getRequiredQsealHeaders();
-  const qsealKey = fs.readFileSync(config.QSEAL_KEY_PATH);
-  const qsealKeyId = 'PSDFR-ACPR-71758';
-
-  if (request.getHeaders().digest) {
-    qsealHeaders.push('digest');
-  }
-
-  httpSignature.sign(request, {
-    authorizationHeaderName: 'Signature',
-    headers: qsealHeaders,
-    key: qsealKey,
-    keyId: qsealKeyId,
-  });
-};
-
-const getRequestHeaders = (method, port = 443) => ({
-  'PSU-Accept':
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-  'PSU-Accept-Charset': 'utf-8',
-  'PSU-Accept-Encoding': 'gzip, deflate, br',
-  'PSU-Accept-Language': 'en-US,en;q=0.9',
-  'PSU-Date': '2019-01-01T00:00:00Z',
-  'PSU-HTTP-Method': method ?? 'GET',
-  'PSU-IP-Address': '127.0.0.1',
-  'PSU-IP-Port': port,
-  'PSU-Referer': `https://localhost:${port}/`,
-  'PSU-User-Agent':
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-});
 
 const doRequest = ({ method, path, authorization, payload }) =>
   new Promise((resolve, reject) => {
@@ -80,23 +25,18 @@ const doRequest = ({ method, path, authorization, payload }) =>
 
     const options = {
       host: SHINE_CONNECT_HOST,
+      port: 10081,
       path,
       method,
       headers: {
-        ...getRequestHeaders(),
         ...(postData
           ? {
               'Content-Length': postData.length,
               'Content-Type': 'application/json',
-              digest: generateDigest(postData, QSEAL_KEY),
             }
           : {}),
         Authorization: `Bearer ${authorization}`,
       },
-      // Client certificates for mutual TLS authentication
-      key: QWAC_KEY,
-      cert: QWAC_CERT,
-      ca: ROOT_CERT,
     };
 
     // Make the HTTPS request
@@ -137,7 +77,6 @@ const doRequest = ({ method, path, authorization, payload }) =>
       reject(e);
     });
 
-    signRequest(req);
     if (postData) {
       req.write(postData);
       req.end();
