7ce94ded53ab7988706c0534c7d060ff082b7ea6
Rabah LAOUADI <rabah.laouadi@shine.fr>
2025-08-19 09:43:08 +0200

    Update getHosts.ts

--- server/config/getHosts.ts
+++ server/config/getHosts.ts
@@ -1,7 +1,7 @@
 const SHINE_AUTHENTICATION_PRODUCTION_URL = 'https://api.shine.fr/v2/authentication';
 const SHINE_AUTHENTICATION_STAGING_URL = 'https://api.staging.shine.fr/v2/authentication';
 
-const SHINE_REGULATED_PRODUCTION_HOST = 'connect.api.shine.fr';
+const SHINE_REGULATED_PRODUCTION_HOST = 'mtls.connect.shine.fr';
 const SHINE_REGULATED_STAGING_HOST = 'mtls.connect.staging.shine.fr';
 const SHINE_REGULATED_DEV_HOST = 'localhost';
 
